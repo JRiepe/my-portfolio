3e40db56273a8b3947af7965e31f64f9f1dab472
John Riepe <johnriepe@gmail.com>
2016-08-18 12:26:37 -0400

    fixed email php

--- email.php
+++ email.php
@@ -2,15 +2,13 @@
 	if (isset($_POST["submit"])) {
 		$name = $_POST['name'];
 		$email = $_POST['email'];
-		$website = $_POST['website'];
 		$message = $_POST['message'];
-		
+		$human = intval($_POST['human']);
 		$from = 'Demo Contact Form'; 
-		$to = 'johnriepe@gmail.com'; 
+		$to = 'example@domain.com'; 
 		$subject = 'Message from Contact Demo ';
 		
-		$body = "From: $name\n E-Mail: $email\n Message:\n $message";
- 
+		$body ="From: $name\n E-Mail: $email\n Message:\n $message";
 		// Check if name has been entered
 		if (!$_POST['name']) {
 			$errName = 'Please enter your name';
@@ -25,14 +23,16 @@
 		if (!$_POST['message']) {
 			$errMessage = 'Please enter your message';
 		}
-		
- 
+		//Check if simple anti-bot test is correct
+		if ($human !== 5) {
+			$errHuman = 'Your anti-spam is incorrect';
+		}
 // If there are no errors, send the email
-if (!$errName && !$errEmail && !$errMessage) {
+if (!$errName && !$errEmail && !$errMessage && !$errHuman) {
 	if (mail ($to, $subject, $body, $from)) {
 		$result='<div class="alert alert-success">Thank You! I will be in touch</div>';
 	} else {
-		$result='<div class="alert alert-danger">Sorry there was an error sending your message. Please try again later</div>';
+		$result='<div class="alert alert-danger">Sorry there was an error sending your message. Please try again later.</div>';
 	}
 }
 	}
